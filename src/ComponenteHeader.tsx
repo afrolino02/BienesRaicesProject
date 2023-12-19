17b629f706ac72e0ce699448ad4471955a014ad2
afrolino02 <andres.puello.gomez@gmail.com>
2023-12-18 19:33:42 -0500

    Repository made

---
+++ src/ComponenteHeader.tsx
@@ -0,0 +1,19 @@
+
+export const ComponenteHeader = () => {
+     return (
+    <header className="header">
+        <div>
+            <img className="logo" src="../public/logo.svg" alt="" />
+        </div>
+        <nav className="navbar">
+            <ul>
+                <li>Inicio</li>
+                <li>nosotros</li>
+                <li>casas</li>
+                <li>contactanos</li>
+            </ul>
+        </nav>
+    </header>
+
+);
+}
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './app.css'
+import { ComponenteHeader } from './ComponenteHeader'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.Fragment>
+    <ComponenteHeader />
+  </React.Fragment>,
+  
+)
